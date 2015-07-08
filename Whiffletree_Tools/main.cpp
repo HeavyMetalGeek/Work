0ed7a9392b68dfc7eb3760eeffffaa079f159fb7
fatirishman53 <fatirishman53@users.noreply.github.com>
2015-07-07 20:00:36 -0500

    Added temporary interface

--- Whiffletree_Tools/main.cpp
+++ Whiffletree_Tools/main.cpp
@@ -1,7 +1,25 @@
 #include "WorkNamespace.h"
-
-int main()
+#include <fstream>
+using namespace work;
+int main(int argc, char *argv[])
 {
-
+    if (argc != 6) return 1;
+    for (int i = 0; i < argc; i++)
+    {
+        string arg = argv[i];
+        if (arg == "-l" || arg == "--lengths")
+        {
+            beam myBeam;
+            myBeam.arm = atof(argv[2]);
+            myBeam.armPoint.load = atof(argv[3]);
+            myBeam.span = atof(argv[4]);
+            myBeam.otherPoint.load = atof(argv[5]);
+            myBeam.lengthsCalc();
+            myBeam.beamSelect();
+            cout << "Beam Type: \t\t" << myBeam.shape << endl;
+            cout << "Arm/Span Ratio: \t" << myBeam.armSpanRatio << endl;
+            cout << "% Allow Shear: \t\t" << myBeam.pctAllowShear << endl;
+        }
+    }
     return 0;
 }
